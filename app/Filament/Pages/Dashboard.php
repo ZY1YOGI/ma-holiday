0bc465e49f6eb615ec882cfa052a576845c85de7
Youssef Amjad <ZY1YOGI@gmail.com>
2023-10-25 20:14:59 +0300

    fix extends BaseDashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,18 +2,13 @@
 
 namespace App\Filament\Pages;
 
-use Filament\Pages\Page;
 use App\Filament\Widgets\StatsOverviewWidget;
-class Dashboard extends Page
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
 {
-    protected static ?string $navigationIcon = 'heroicon-o-home';
 
-    protected static string $view = 'filament.pages.dashboard';
-
-    protected static ?string $slug = '/';
-
-
-    protected  static function getWidgets(): array
+    public function getWidgets(): array
     {
         return [
             StatsOverviewWidget::class,
