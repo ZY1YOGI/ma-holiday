8f82bc4b3db3d61db8796f662ab17db10422c377
Youssef Amjad <ZY1YOGI@gmail.com>
2023-12-02 18:00:38 +0200

    work in auth

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,17 @@
+<x-layout title="Login" description="Login description" keywords="Login keywords">
+    @include('partials.header')
+
+
+
+
+
+
+    <main class="min-h-screen">
+        <h1>Login</h1>
+    </main>
+
+
+
+
+    @include('partials.footer')
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+
+
+
+
+
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
