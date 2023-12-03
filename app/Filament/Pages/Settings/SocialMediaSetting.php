801c0381708a8617ff8f763825d0602cd172052e
Youssef Amjad <ZY1YOGI@gmail.com>
2023-12-04 00:29:10 +0200

    fix settings

--- app/Filament/Pages/Settings/SocialMediaSetting.php
+++ app/Filament/Pages/Settings/SocialMediaSetting.php
@@ -22,15 +22,13 @@ class SocialMediaSetting extends SettingsPage
                     ->schema([
                         Forms\Components\TextInput::make('facebook_name')
                             ->label('Facebook Name Page')
-                            ->prefixIcon('bi-facebook')
-                            ->required(),
+                            ->prefixIcon('bi-facebook'),
 
                         Forms\Components\TextInput::make('facebook_url')
                             ->label('Facebook URL Page')
                             ->prefix('https://')
                             ->suffixIcon('heroicon-m-globe-alt')
-                            ->url()
-                            ->required(),
+                            ->url(),
 
                     ])->columns(2),
 
@@ -38,15 +36,13 @@ class SocialMediaSetting extends SettingsPage
                     ->schema([
                         Forms\Components\TextInput::make('instagram_name')
                             ->label('Instagram Name Page')
-                            ->prefixIcon('bi-instagram')
-                            ->required(),
+                            ->prefixIcon('bi-instagram'),
 
                         Forms\Components\TextInput::make('instagram_url')
                             ->label('Instagram URL Page')
                             ->prefix('https://')
                             ->suffixIcon('heroicon-m-globe-alt')
-                            ->url()
-                            ->required(),
+                            ->url(),
 
                     ])->columns(2),
 
@@ -54,15 +50,13 @@ class SocialMediaSetting extends SettingsPage
                     ->schema([
                         Forms\Components\TextInput::make('tiktok_name')
                             ->label('Tiktok Name Page')
-                            ->prefixIcon('bi-tiktok')
-                            ->required(),
+                            ->prefixIcon('bi-tiktok'),
 
                         Forms\Components\TextInput::make('tiktok_url')
                             ->label('Tiktok URL Page')
                             ->prefix('https://')
                             ->suffixIcon('heroicon-m-globe-alt')
-                            ->url()
-                            ->required(),
+                            ->url(),
 
                     ])->columns(2),
 
@@ -70,15 +64,13 @@ class SocialMediaSetting extends SettingsPage
                     ->schema([
                         Forms\Components\TextInput::make('twitter_name')
                             ->label('Twitter Name Page')
-                            ->prefixIcon('bi-twitter')
-                            ->required(),
+                            ->prefixIcon('bi-twitter'),
 
                         Forms\Components\TextInput::make('twitter_url')
                             ->label('Twitter URL Page')
                             ->prefix('https://')
                             ->suffixIcon('heroicon-m-globe-alt')
-                            ->url()
-                            ->required(),
+                            ->url(),
 
                     ])->columns(2),
 
@@ -86,14 +78,12 @@ class SocialMediaSetting extends SettingsPage
                     ->schema([
                         Forms\Components\TextInput::make('gmail_name')
                             ->label('E-mail Name')
-                            ->prefixIcon('heroicon-o-envelope')
-                            ->required(),
+                            ->prefixIcon('heroicon-o-envelope'),
 
                         Forms\Components\TextInput::make('gmail_url')
                             ->label('E-mail URL')
                             ->prefix('E-mai')
-                            ->email()
-                            ->required(),
+                            ->email(),
 
                     ])->columns(2),
 
@@ -103,15 +93,13 @@ class SocialMediaSetting extends SettingsPage
                             ->label('WhatsApp Phone 1')
                             ->prefixIcon('bi-whatsapp')
                             ->tel()
-                            ->telRegex('/\+?([0-9]{2})-?([0-9]{3})-?([0-9]{6,7})/')
-                            ->required(),
+                            ->telRegex('/\+?([0-9]{2})-?([0-9]{3})-?([0-9]{6,7})/'),
 
                         Forms\Components\TextInput::make('whatsapp_phone_2')
                             ->label('WhatsApp Phone 2')
                             ->prefixIcon('bi-whatsapp')
                             ->tel()
-                            ->telRegex('/\+?([0-9]{2})-?([0-9]{3})-?([0-9]{6,7})/')
-                            ->required(),
+                            ->telRegex('/\+?([0-9]{2})-?([0-9]{3})-?([0-9]{6,7})/'),
 
                     ])->columns(2)
             ]);
--- database/settings/2023_12_02_113516_create_social_media_settings.php
+++ database/settings/2023_12_02_113516_create_social_media_settings.php
@@ -6,22 +6,22 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('social_media.facebook_name', '');
-        $this->migrator->add('social_media.facebook_url', '');
+        $this->migrator->add('social_media.facebook_name');
+        $this->migrator->add('social_media.facebook_url');
 
-        $this->migrator->add('social_media.instagram_name', '');
-        $this->migrator->add('social_media.instagram_url', '');
+        $this->migrator->add('social_media.instagram_name');
+        $this->migrator->add('social_media.instagram_url');
 
-        $this->migrator->add('social_media.tiktok_name', '');
-        $this->migrator->add('social_media.tiktok_url', '');
+        $this->migrator->add('social_media.tiktok_name');
+        $this->migrator->add('social_media.tiktok_url');
 
-        $this->migrator->add('social_media.twitter_name', '');
-        $this->migrator->add('social_media.twitter_url', '');
+        $this->migrator->add('social_media.twitter_name');
+        $this->migrator->add('social_media.twitter_url');
 
-        $this->migrator->add('social_media.gmail_name', '');
-        $this->migrator->add('social_media.gmail_url', '');
+        $this->migrator->add('social_media.gmail_name');
+        $this->migrator->add('social_media.gmail_url');
 
-        $this->migrator->add('social_media.whatsapp_phone_1', '');
-        $this->migrator->add('social_media.whatsapp_phone_2', '');
+        $this->migrator->add('social_media.whatsapp_phone_1');
+        $this->migrator->add('social_media.whatsapp_phone_2');
     }
 };
