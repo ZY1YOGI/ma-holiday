d08a1da845f6153db6d4d2a856d923f0736c88f0
Youssef Amjad <ZY1YOGI@gmail.com>
2023-12-04 00:29:39 +0200

    fix settings

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -7,7 +7,7 @@ use Spatie\LaravelSettings\Settings;
 
 class GeneralSettings extends Settings
 {
-    public string $default_theme;
+    public string|null $default_theme;
 
 
     public static function group(): string
--- app/Settings/SocialMediaSettings.php
+++ app/Settings/SocialMediaSettings.php
@@ -7,17 +7,17 @@ use Spatie\LaravelSettings\Settings;
 
 class SocialMediaSettings extends Settings
 {
-    public string $facebook_name, $facebook_url;
+    public string|null $facebook_name, $facebook_url;
 
-    public string $instagram_name, $instagram_url;
+    public string|null $instagram_name, $instagram_url;
 
-    public string $tiktok_name, $tiktok_url;
+    public string|null $tiktok_name, $tiktok_url;
 
-    public string $twitter_name, $twitter_url;
+    public string|null $twitter_name, $twitter_url;
 
-    public string $gmail_name, $gmail_url;
+    public string|null $gmail_name, $gmail_url;
 
-    public string $whatsapp_phone_1, $whatsapp_phone_2;
+    public string|null $whatsapp_phone_1, $whatsapp_phone_2;
 
     public static function group(): string
     {
