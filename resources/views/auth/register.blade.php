f22c91d8648b5c95ebbc892136d83bd5cc45d27c
Youssef Amjad <ZY1YOGI@gmail.com>
2023-12-04 21:32:15 +0200

    fix name input

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,7 +21,7 @@
                         </div>
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="exmpale@email.com" value="{{ old('email') }}" required autocomplete="new-email">
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@email.com" value="{{ old('email') }}" required autocomplete="new-email">
                             <span class="text-red-500 pt-1.5 pl-1.5 inline-block">{{ $errors->first('email') }}</span>
                         </div>
                         <div>
